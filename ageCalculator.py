a190f6a31a8ccbc11490f38e7a8855b298830104
deeptilobo <lobo.deepti@gmail.com>
2017-09-27 07:09:39 +0530

    Update ageCalculator.py

--- ageCalculator.py
+++ ageCalculator.py
@@ -2,12 +2,13 @@ import datetime
 
 now = datetime.datetime.now()
 
-name = input("enter your name : ")
-age = int(input("enter you age : ")
+name = raw_input("enter your name : ")
+
+age = int(input("enter you age : "))
 
 n1 = 100 - age
 yr = now.year
 year = yr + n1
 
 print("Hi " + name)
-print("you will turn 100yrs in the year : " + str(year)
+print("you will turn 100yrs in the year : " + str(year))
